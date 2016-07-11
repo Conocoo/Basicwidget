6974a4f75b28564ac266d6d7949f504c8b238cc5
YoungGyun Lee <yk1226ull@gmail.com>
2016-07-11 17:50:24 +0900

    Initial commit

---
+++ app/src/main/java/com/dodream/www/basicwidget/a_real.java
@@ -0,0 +1,13 @@
+package com.dodream.www.basicwidget;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class a_real extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.a_real);
+    }
+}
