ddb70df65962c9b91dabc6efcdf69b8792bf037d
YoungGyun Lee <yk1226ull@gmail.com>
2016-07-12 11:55:03 +0900

    commit

---
+++ app/src/main/java/com/dodream/www/basicwidget/a_text.java
@@ -0,0 +1,13 @@
+package com.dodream.www.basicwidget;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class a_text extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.a_text);
+    }
+}
